30ee13c42a2856a592b106f17f8d149b53b77431
Orsolya Dekany <orsolya.dekany@liferay.com>
2021-09-05 14:11:59 +0200

    get all articles

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/wikiDB", {userNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
 
 const articleSchema = {
     title: String,
@@ -23,7 +23,15 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-//TODO
+app.get("/articles", function (req, res) {
+    Article.find(function(err, foundArticles){
+        if (!err) {
+            res.send(foundArticles);
+        } else {
+            res.send(err);
+        }
+    });
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
